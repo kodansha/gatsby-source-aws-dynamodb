1dcc1381a7e77033261c2136c58b7932f2ffe7b7
mottox2 <y.mottox2@gmail.com>
2021-03-05 16:52:03 +0900

    Use cjs module

---
+++ gatsby-node.js
@@ -0,0 +1,47 @@
+const AWS = require("aws-sdk");
+const { ERROR_MAP } = require(`./src/error-utils`);
+const { processData } = require(`./src/utils`);
+
+exports.onPreInit = ({ reporter }) => {
+  if (reporter.setErrorMap) {
+    reporter.setErrorMap(ERROR_MAP);
+  }
+};
+
+exports.sourceNodes = (
+  { actions, createNodeId, createContentDigest },
+  options
+) => {
+  return new Promise((resolve, reject) => {
+    const { createNode } = actions;
+    delete options.plugins;
+
+    const onScan = (err, data) => {
+      if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        console.log("Query succeeded.");
+        data.Items.forEach((item) => {
+          const nodeData = processData(item, createNodeId, createContentDigest);
+          createNode(nodeData);
+        });
+
+        if (typeof data.LastEvaluatedKey != "undefined") {
+          console.log("Scanning for more...");
+          options.params.ExclusiveStartKey = data.LastEvaluatedKey;
+          docClient.scan(options.params, onScan);
+        } else {
+          resolve();
+        }
+      }
+    };
+
+    const docClient = new AWS.DynamoDB.DocumentClient({
+      region: options.region,
+      accessKeyId: options.accessKeyId,
+      secretAccessKey: options.secretAccessKey,
+    });
+    docClient.scan(options.params, onScan);
+  });
+};
--- src/utils.js
+++ src/utils.js
@@ -1,22 +1,21 @@
 const processData = (item, createNodeId, createContentDigest) => {
-    const nodeContentDigest = createContentDigest(item)
-    const nodeId = item.id
-      ? createNodeId(`dynamodb-${item.id}`)
-      : nodeContentDigest
-    
-    const nodeData = Object.assign({}, item, {
-      id: nodeId,
-      parent: null,
-      children: [],
-      internal: {
-        mediaType: `text/html`,
-        contentDigest: nodeContentDigest,
-        type: options.typeName,
-        content: JSON.stringify(item),
-      },
-    })
-    return nodeData
-}
-  export {
-      processData
-  }
+  const nodeContentDigest = createContentDigest(item);
+  const nodeId = item.id
+    ? createNodeId(`dynamodb-${item.id}`)
+    : nodeContentDigest;
+
+  const nodeData = Object.assign({}, item, {
+    id: nodeId,
+    parent: null,
+    children: [],
+    internal: {
+      mediaType: `text/html`,
+      contentDigest: nodeContentDigest,
+      type: options.typeName,
+      content: JSON.stringify(item),
+    },
+  });
+  return nodeData;
+};
+
+module.exports = { processData };
